9bb9ba1175a85d934dfbe5892220cc761ad88db1
SatanSaint <wasin.sai@mail.kmutt.ac.th>
2021-08-27 14:25:08 +0700

    Update run_newman.sh

--- run_newman.sh
+++ run_newman.sh
@@ -1,6 +1,8 @@
 rm -r ./allure-results
 mockoon-cli start --data ./FileMockoon/testmockdata.json
 newman run ./file_json/testmockdata.postman_collection.json -r allure
-# allure serve ./allure-results
 allure generate --clean ./allure-results
 mockoon-cli stop all
+git add .
+git commit -m "update report"
+git push origin master
