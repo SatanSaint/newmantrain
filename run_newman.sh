2ef00e15f558cc051f516db3783a4c4fc35f8360
Mil:D <mild@MilDs-MacBook-Pro.local>
2021-08-27 14:07:52 +0700

    update gen report

--- run_newman.sh
+++ run_newman.sh
@@ -1,6 +1,6 @@
 rm -r ./allure-results
 mockoon-cli start --data ./FileMockoon/testmockdata.json
 newman run ./file_json/testmockdata.postman_collection.json -r allure
-allure serve ./allure-results
-# allure generate --clean ./allure-results
+# allure serve ./allure-results
+allure generate --clean ./allure-results
 mockoon-cli stop all
